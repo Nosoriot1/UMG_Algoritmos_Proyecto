792ae5c7a43a4e8b586a8cb0c92b1f89de8a4d57
Nosoriot1 <143118561+Nosoriot1@users.noreply.github.com>
2023-10-10 20:56:30 -0600

    Prueba 2
    se hizo cambio 2

--- UMG_Proyecto_Algoritmos.cpp
+++ UMG_Proyecto_Algoritmos.cpp
@@ -1,15 +1,13 @@
-#Include <iostream>
+#include <iostream>
 
-<<<<<<< Updated upstream
 using namespace std;
 
 main(){
 
     cout<<"Proyecto";
-}
-=======
     cout<<"Proyecto"<<endl;
     cout<<"Hola Mundo"<<endl;
     cout<<"Realizando pruebas"<<endl;
+    cout<<"prueba"<<endl;
 }
->>>>>>> Stashed changes
+
